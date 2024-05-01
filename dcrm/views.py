20dbdd956ae1fdad47e7ee4133b49c12db980abe
JomarLomocso <jmrlmcso0801@gmail.com>
2024-05-01 20:02:52 +0800

    Third commit

--- dcrm/views.py
+++ dcrm/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 
 # Create your views here.
 
 def home(request):
 
     return render(request, 'home.html')
+
+def register(request):
+#do something here
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password =  form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+
+
+    return render(request, 'register.html')
--- lomocso/urls.py
+++ lomocso/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('dcrm.urls')),
+    path('', include("dcrm.urls")),
 ]
