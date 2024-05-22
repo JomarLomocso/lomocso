4d3d5f466893210158e89517615b152800ba610c
JomarLomocso <jmrlmcso0801@gmail.com>
2024-05-22 15:32:01 +0800

    database change

--- lomocso/settings.py
+++ lomocso/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'lomocso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ipat2b',
+        'USER': 'postgres',
+        'PASSWORD': 'lomocso',
+        'PORT': '5432',
     }
 }
 
